eec7098cdd4d154e0321f17e5bde4cd0d23f9df9
Tikshan Mohan <tik.mohan11@gmail.com>
2022-07-08 11:22:20 +0200

    Done some exception handling

--- Merge_PDF.py
+++ Merge_PDF.py
@@ -1,3 +1,4 @@
+# Importing useful modules
 import sys
 from PyPDF2 import PdfFileMerger
 
@@ -6,13 +7,28 @@ pdf_merger = PdfFileMerger()
 
 input_files = []
 
-
-for arg in sys.argv[1:]:
+# Getting the input files from the command line
+for arg in sys.argv[1:len(sys.argv)-1]:
    input_files.append(arg)
 
+# Getting the name of output file from the command line
+out_file_name = sys.argv[-1]
 
+
+# Merging the files
 for file in input_files:
-    pdf_merger.append(file)
+    try:
+        pdf_merger.append(file)
+    except Exception:
+        print("Please Provide PDFs Only!")
+        sys.exit(1)
 
-pdf_merger.write('result.pdf')
+
+# Saving the result into the output file
+try:
+    pdf_merger.write(out_file_name)
+except Exception:
+    print('The name for the output file already exists! Please pick a different name')
+    sys.exit(1)
+
 pdf_merger.close()
