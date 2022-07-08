748a59fb52406d819d09c7a540b23f5ba24a6381
Tikshan Mohan <tik.mohan11@gmail.com>
2022-07-08 18:52:41 +0200

    PNG_To_PDF: Added the exception handling

--- PNG_To_PDF.py
+++ PNG_To_PDF.py
@@ -1,27 +1,46 @@
+# Importing useful modules
 import argparse
 from PIL import Image
+import sys
 
-
+# Parsing the command line
 parser = argparse.ArgumentParser(description='Add Images To Merge Into A Single PDF')
-parser.add_argument('--s','--sort',dest='sort',help='Sort the input files')
+parser.add_argument('-s','--sort',dest='sort',action='store_true',help='Sort the input files')
 parser.add_argument('files',nargs='*')
+parser.add_argument('-o','--output',dest='output', help='Output file name')
+
 
 args = parser.parse_args()
 
 
-output = []
 files = []
+output = []
 
+# Saving the image files into an array
 for arg in args.files:
     files.append('%s'%(arg))
 
+# If the sort flag is on, the array gets sorted
 if args.sort:
     files.sort()
 
-# Testing
-for file in files:
-    img = Image.open(file)
-    im = img.convert('RGB')
-    output.append(im)
+# Default name for the output file is result.pdf, but if the output flag is given, then use the user definied name
+output_name = 'result.pdf'
+if args.output:
+    output_name = args.output
 
-output[0].save(r'./my_images.pdf', save_all=True, append_images=output[1:])
+# Saving the images in an array
+for file in files:
+    try:
+        img = Image.open(file)
+        im = img.convert('RGB')
+        output.append(im)
+    except:
+        print('Only Images Allowed!')
+        sys.exit(1)
+
+# Saving the output
+try:
+    output[0].save(output_name, save_all=True, append_images=output[1:])
+except:
+    print("File name should have the pdf extenstion!")
